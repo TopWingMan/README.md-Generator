23df38d5c409c783da07a5e1db5cb388a925f2d4
Zachary Stanaford <ZacharyStanaford@gmail.com>
2023-01-07 11:09:42 -0800

    initial push

---
+++ index.js
@@ -0,0 +1,13 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = ['Project titile', 'Description', 'Installation instructions', 'Usage information', 'Liscense', 'Contributing', 'Test instructions', 'Github username', 'Email address'];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
